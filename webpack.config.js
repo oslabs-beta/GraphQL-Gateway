49249a3eb4ebda50c5a1b1f81febbf6e96d5dfa7
Stephan Halarewicz <shalarewicz@gmail.com>
2022-05-18 10:22:08 -0400

    Updated typescript + eslint config. Prettified

--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,78 @@
-const path = require("path");
-const HTMLWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "client", "index.tsx"),
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-  },
-  mode: process.env.NODE_ENV,
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        exclude: "/node_modules/",
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.html$/,
-        use: ['html-loader']
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        exclude: "/node_modules/",
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(jpe?g|gif|png|svg)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "img/",
-              publicPath: "img/",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(jpe?g|gif|png|svg)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000
-            }
-          }
-        ]
-      },
-    ],
-  },
-  plugins: [
-    new HTMLWebPackPlugin({ template: "./public/index.html" }),
-    new MiniCssExtractPlugin(),
-  ],
-
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  }
-  ,
-
-  devServer: {
-    // add all routes here
-    proxy: {
+    entry: path.resolve(__dirname, 'client', 'index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
     },
-    open: false,
-    hot: true,
-  },
+    mode: process.env.NODE_ENV,
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: '/node_modules/',
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(jpe?g|gif|png|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(jpe?g|gif|png|svg)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebPackPlugin({ template: './public/index.html' }),
+        new MiniCssExtractPlugin(),
+    ],
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devServer: {
+        // add all routes here
+        proxy: {},
+        open: false,
+        hot: true,
+    },
 };
