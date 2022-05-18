bf4a6c896d0acd95186686830f502507d31e0856
Jon Dewey <donjewey@gmail.com>
2022-05-17 18:25:10 -0700

    updated userSchema to remove unneccesary rows

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,13 +1,7 @@
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  handle: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  firstName: {type: String, required: true},
-  lastName: {type: String, required: true},
-  bio: {type: String, required: false},
-  listings: {type: Number, required: false}
-
+    email: { type: String, required: true, unique: true },
 });
 
 module.exports = mongoose.model('User', userSchema, 'users');
