603ada27f729db9f440beb9ec4d1fbd66fbf4bda
Jon Dewey <donjewey@gmail.com>
2022-05-21 12:03:07 -0700

    updates resolvers to query by id instead of email;

--- server/models/Resolvers.ts
+++ server/models/Resolvers.ts
@@ -3,33 +3,33 @@ import User from './User';
 const bcrypt = require('bcryptjs');
 
 type QueryUserArgs = {
-    email: string;
+    id: Number | String | undefined;
 };
 
 type CreateUserArgs = {
     input: { email: string; password: string };
 };
 
-type EmailArg = {
-    email: string;
+type UpdateUserArgs = {
+    input: {
+        id: Number;
+        email: String;
+        password: String;
+    };
 };
 
 type User = {
+    id: Number;
     email: String;
+    password: String;
 };
 
 const resolvers = {
     Query: {
-        users: () =>
-            // placeholder
-            [
-                { id: '1', email: '1' },
-                { id: '2', email: '2' },
-            ],
         user: async (parent: undefined, args: QueryUserArgs) => {
-            const { email } = args;
+            const { id } = args;
 
-            return User.findOne({ email })
+            return User.findOne({ id })
                 .then((user: User) => {
                     if (!user) return new Error('User does not exist');
                     return { email: user.email };
@@ -54,20 +54,20 @@ const resolvers = {
                 .then((newUser: User) => newUser)
                 .catch(() => new Error('DB query failed'));
         },
-        updateUser: async (parent: undefined, args: EmailArg) => {
-            const { email } = args;
+        updateUser: async (parent: undefined, args: UpdateUserArgs) => {
+            const { id } = args.input;
 
             // try-catch db query here
 
             // placeholder
-            return { email };
+            return { id };
         },
-        deleteUser: async (parent: undefined, args: EmailArg) => {
-            const { email } = args;
+        deleteUser: async (parent: undefined, args: QueryUserArgs) => {
+            const { id } = args;
             // try-catch db query here
 
             // placeholder
-            return { email };
+            return { id };
         },
     },
 };
