2932f8de689f86af1cdcf038c6e14248a515ee2b
Milos Popovic <milos_popovic@outlook.com>
2022-06-19 19:07:07 -0400

    presentation branch

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -1,57 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export interface ISState {
-    user: {
-        email: string;
-        password: string;
-    };
+    viewDashboard: any;
 }
 
-function Login() {
-    const [user, setUser] = useState<ISState['user']>({
-        email: '',
-        password: '',
-    });
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    const handleClick = () => {
-        console.log(user);
-    };
-
-    return (
-        <div className="app">
-            <h1>Login</h1>
-            <div className="login-box">
-                <div className="form-wrapper">
-                    <input
-                        className="input"
-                        type="text"
-                        onChange={handleChange}
-                        name="email"
-                        value={user.email}
-                        placeholder="enter email"
-                    />
-                    <input
-                        className="input"
-                        type="password"
-                        onChange={handleChange}
-                        name="password"
-                        value={user.password}
-                        placeholder="enter password"
-                    />
-                    <button className="btn" type="submit" onClick={handleClick}>
-                        Login
-                    </button>
-                </div>
+// eslint-disable-next-line react/function-component-definition
+const Login: React.FC<ISState> = ({ viewDashboard }) => (
+    <div className="app">
+        <h1>Login</h1>
+        <div className="login-box">
+            <div className="form-wrapper">
+                <input className="input" type="text" name="email" placeholder="enter email" />
+                <input
+                    className="input"
+                    type="password"
+                    name="password"
+                    placeholder="enter password"
+                />
+                <button className="btn" type="submit" onClick={viewDashboard}>
+                    Login
+                </button>
             </div>
         </div>
-    );
-}
+    </div>
+);
 
 export default Login;
