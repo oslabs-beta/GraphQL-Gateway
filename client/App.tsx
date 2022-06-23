42ae2b13f752e8b8868976c1ed0391fb5191f165
Milos Popovic <milos_popovic@outlook.com>
2022-06-23 19:27:12 -0400

    fixed minor bugs, few things left

--- client/App.tsx
+++ client/App.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // import { Button, AppBar, Box, Toolbar, IconButton, Grid, Typography } from '@mui/material';
 import settings from '../public/settings.png';
+import intime from '../public/intime.png';
+import data from '../public/data.png';
+import barchart from '../public/barchart.png';
+import gear from '../public/gear.png';
+// import mockChart from '../public/mockChart.jpg';
 import '../public/styles.css';
 
-// const settings = require('../public/settings.png');
-const intime = require('../public/intime.png');
-const data = require('../public/data.png');
-const barchart = require('../public/barchart.png');
-
 export default function HomePage() {
+    // this is a temp solution to hide the navbar. Final one will hide on scroll up
+    const [show, setShow] = useState('block');
+    const controlNavbar = () => {
+        if (window.scrollY > 100) {
+            setShow('none');
+        } else {
+            setShow('block');
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', controlNavbar);
+        return () => {
+            window.removeEventListener('scroll', controlNavbar);
+        };
+    }, []);
+
     const navigate = useNavigate();
 
     const handleLogin = (): void => {
@@ -30,7 +47,7 @@ export default function HomePage() {
 
     return (
         <div>
-            <div id="navbar">
+            <div id="navbar" style={{ display: show }}>
                 {/* <img src="logo.png" alt="logo" /> */}
 
                 <button type="submit" onClick={handleHome} className="linkBtn">
@@ -43,7 +60,7 @@ export default function HomePage() {
                     About
                 </button>
             </div>
-            <div id="side-navbar">
+            <div id="side-navbar" style={{ display: show }}>
                 <button type="submit" onClick={handleLogin} className="linkBtn">
                     Login
                 </button>
@@ -64,23 +81,64 @@ export default function HomePage() {
                         </p>
                     </div>
                 </section>
-                <section id="demoCard" />
+                <section id="demoCard">
+                    <div id="demoCardWrapper">
+                        <div className="algorithmCard">
+                            <h3>Alghorhitm</h3>
+                            <h1>Token Bucket</h1>
+                            <img src={gear} alt="settings" />
+                        </div>
+                        <div className="algorithmCard">
+                            <h3>Alghorhitm</h3>
+                            <h1>Sliding Window</h1>
+                            <img src={gear} alt="settings" />
+                        </div>
+                        <div className="algorithmCard">
+                            <h3>Alghorhitm</h3>
+                            <h1>Leacky Bucket</h1>
+                            <img src={gear} alt="settings" />
+                        </div>
+                        <div className="demoCardImg">
+                            {/* <img className="iconImg" src={mockChart} alt="mockChart" /> */}
+                        </div>
+                    </div>
+                </section>
                 <section id="pageTwo" />
                 <section id="pageThree">
-                    <div>
-                        <div id="pageThreeLeft" />
+                    <div id="pageThreeWrapper">
+                        <div className="centeredSentence">
+                            <h1>What makes us different?</h1>
+                        </div>
+                        <div id="pageThreeLeft">
+                            <div className="icon">
+                                <img className="iconImg" src={settings} alt="settings-icon" />
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </div>
+                            <div className="icon">
+                                <img className="iconImg" src={barchart} alt="barchart-icon" />
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </div>
+                        </div>
                         <div id="pageThreeRight">
                             <div className="icon">
-                                <img src={settings} alt="settings-icon" />
+                                <img className="iconImg" src={intime} alt="intime-icon" />
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
                             </div>
                             <div className="icon">
-                                <img src={barchart} alt="barchart-icon" />
-                            </div>
-                            <div className="icon">
-                                <img src={intime} alt="intime-icon" />
-                            </div>
-                            <div className="icon">
-                                <img src={data} alt="data-icon" />
+                                <img className="iconImg" src={data} alt="data-icon" />
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
                             </div>
                         </div>
                     </div>
