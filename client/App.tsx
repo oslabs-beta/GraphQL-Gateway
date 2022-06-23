3c59f44dc8c94d4e0522abc1e6d7bad21ffe1f86
Milos Popovic <milos_popovic@outlook.com>
2022-06-22 20:31:55 -0400

    update 8:30PM

--- client/App.tsx
+++ client/App.tsx
@@ -3,6 +3,11 @@ import { useNavigate } from 'react-router-dom';
 // import { Button, AppBar, Box, Toolbar, IconButton, Grid, Typography } from '@mui/material';
 import '../public/styles.css';
 
+const settings = require('../public/settings.png');
+const intime = require('../public/intime.png');
+const data = require('../public/data.png');
+const barchart = require('../public/barchart.png');
+
 export default function HomePage() {
     const navigate = useNavigate();
 
@@ -46,9 +51,39 @@ export default function HomePage() {
                 </button>
             </div>
             <div className="container">
-                <section id="pageOne" />
+                <section id="pageOne">
+                    <div id="pageOneTextBox">
+                        <h1>Graph QL Rate Limiter</h1>
+                        <h2>Gateway visualisation tool</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            commodo consequat.
+                        </p>
+                    </div>
+                </section>
+                <section id="demoCard" />
                 <section id="pageTwo" />
-                <section id="pageThree" />
+                <section id="pageThree">
+                    <div>
+                        <div id="pageThreeLeft" />
+                        <div id="pageThreeRight">
+                            <div className="icon">
+                                <img src={settings} alt="settings-icon" />
+                            </div>
+                            <div className="icon">
+                                <img src={barchart} alt="barchart-icon" />
+                            </div>
+                            <div className="icon">
+                                <img src={intime} alt="intime-icon" />
+                            </div>
+                            <div className="icon">
+                                <img src={data} alt="data-icon" />
+                            </div>
+                        </div>
+                    </div>
+                </section>
                 <div id="footer">
                     <div>
                         <h4>Copyright © Team Cat-Snake 2022</h4>
