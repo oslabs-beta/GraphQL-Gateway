0a36f7d5850d310eb5192ad414a76be756dc98f0
[Evan McNeely] <evanmcneely@me.com>
2022-07-07 20:28:12 -0700

    added a loading component

---
+++ client/components/Loading.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Loading() {
+    return <div id="loading">LOADING...</div>;
+}
--- client/components/RequireAuth.tsx
+++ client/components/RequireAuth.tsx
@@ -1,13 +1,14 @@
 import React, { PropsWithChildren } from 'react';
 import { useLocation, Navigate } from 'react-router-dom';
 import { useAuth } from '../auth/AuthProvider';
+import Loading from './Loading';
 
 function RequireAuth({ children }: PropsWithChildren<unknown>) {
     const { user, loading } = useAuth();
     const location = useLocation();
     // if authentication is still being confirmed, return a loading component
     // TODO: replace this with a loading component
-    if (loading) return <div>Loading...</div>;
+    if (loading) return <Loading />;
     // eslint-disable-next-line react/jsx-no-useless-fragment
     return user ? <>{children}</> : <Navigate to="/" state={{ from: location }} replace />;
 }
