94ba57af4be1829af18edd58833c12b58e5f1831
[Evan McNeely] <evanmcneely@me.com>
2022-07-15 12:52:05 -0700

    Chart can be set up with different smoothing factors and time ranges and is displaying depth, complexity, tokens and blocked rate

--- client/components/ChartBox.tsx
+++ client/components/ChartBox.tsx
@@ -36,10 +36,11 @@ export const options = {
 };
 export interface ISState {
     style: {
-        // chartOne: string;
+        chartOne: string;
         chartTwo: string;
         chartThree: string;
         chartFour: string;
+        chartFive: string;
     };
 }
 
@@ -49,69 +50,124 @@ export interface IProps {
 
 // eslint-disable-next-line react/function-component-definition
 const ChartBox: React.FC<IProps> = ({ queries }) => {
-    // const timeFromProjectProps: number[] = [];
-    // const depthFromProjectProps: number[] = [];
-    // const complexityFromProjectProps: number[] = [];
-    // const labels: string[] = [];
-
+    /** create the state required for the chart */
     const [depthData, setDepthData] = useState<number[]>([]);
-    const [complexityData, setcomplexityData] = useState<number[]>([]);
+    const [complexityData, setComplexityData] = useState<number[]>([]);
+    const [tokenData, setTokenData] = useState<number[]>([]);
+    const [blockedData, setblockedData] = useState<number[]>([]);
     const [labels, setLabels] = useState<string[]>([]);
+    const [smoothingFactor, setSmoothingFactor] = useState<1 | 2 | 4 | 8 | 10>(8);
+    const [timeRangeDays, setTimeRangeDays] = useState<30 | 90 | 180 | 360>(90);
+
+    /** useEffect will create the chart data to display form the query data */
     useEffect(() => {
+        /** create storage for the */
         const depthArray: number[] = [];
         const complexityArray: number[] = [];
+        const tokenArray: number[] = [];
+        const blockedArray: number[] = [];
         const labelsArray: string[] = [];
+
+        /** layout the begining of the chart and time increments for each point */
         const currentTime = new Date().valueOf();
-        const timeBlock = 1800000 * 4; // 60 minutes
+        const timeBlock = 1800000 * smoothingFactor; // 15 minutes * the smoothing factor applied
         let nextTimeBlock = queries[0].timestamp + timeBlock;
+        const startTime = currentTime - timeRangeDays * 86400000; // 1 day * number of days for the time frame
+
+        /** This is a helper function to pad the start and finish of the time block if no queries exist */
+        const padChartRange = (from: number, to: number): void => {
+            let current = from;
+            while (current < to) {
+                labelsArray.push(new Date(current).toDateString());
+                depthArray.push(0);
+                complexityArray.push(0);
+                tokenArray.push(0);
+                blockedArray.push(0);
+                current += timeBlock;
+            }
+        };
+
+        /** pad the start of the time range */
+        padChartRange(startTime, queries[0].timestamp);
+
+        /** interate through the queries, averaging the query data that falls within the same time block */
         for (let i = 0; i < queries.length; i += 1) {
-            labelsArray.push(String(queries[i].timestamp));
+            labelsArray.push(new Date(queries[i].timestamp).toDateString());
             let count = 0;
             let totalDepth = 0;
             let totalComplexity = 0;
+            let totalTokens = 0;
+            let totalBlocked = 0;
             while (i < queries.length && queries[i].timestamp < nextTimeBlock) {
                 totalDepth += queries[i].depth;
                 totalComplexity += queries[i].complexity;
+                totalTokens += queries[i].tokens;
+                if (!queries[i].success) totalBlocked += 1;
                 count += 1;
                 i += 1;
             }
             i -= 1;
             depthArray.push(Math.round(totalDepth / count) || 0);
             complexityArray.push(Math.round(totalComplexity / count) || 0);
+            tokenArray.push(Math.round(totalTokens / count) || 0);
+            blockedArray.push(Math.round((totalBlocked / count) * 100) || 0);
             nextTimeBlock += timeBlock;
         }
+
+        /** pad the end of the time range, from the last query to the curretn time */
+        padChartRange(queries[queries.length - 1].timestamp, currentTime);
+
+        /** set the state of the chart data */
         setDepthData(depthArray);
-        setcomplexityData(complexityArray);
+        setComplexityData(complexityArray);
+        setTokenData(tokenArray);
+        setblockedData(blockedArray);
         setLabels(labelsArray);
-    }, [queries]);
+    }, [queries, timeRangeDays, smoothingFactor]);
 
-    // eslint-disable-next-line array-callback-return
-    // queries!.map((query) => {
-    //     // timeFromProjectProps.push(query.timestamp / 100);
-    //     depthFromProjectProps.push(query.depth);
-    //     complexityFromProjectProps.push(query.complexity);
-    // });
+    /** Configure the datasets for Chart.js */
+    const tokens = {
+        labels,
+        datasets: [
+            {
+                type: 'line' as const,
+                label: 'Tokens',
+                borderColor: 'rgba(255, 99, 132, 0.5)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                borderWidth: 2,
+                fill: false,
+                data: tokenData,
+                tension: 0.5,
+                pointStyle: 'line',
+            },
+        ],
+    };
 
-    // const time = {
-    //     labels,
-    //     datasets: [
-    //         {
-    //             label: 'Time to execute query',
-    //             data: timeFromProjectProps,
-    //             borderColor: 'rgb(255, 99, 132)',
-    //             backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    //         },
-    //     ],
-    // };
+    const blocked = {
+        labels,
+        datasets: [
+            {
+                type: 'line' as const,
+                label: 'Blocked',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                borderColor: 'rgba(53, 162, 235, 0.5)',
+                data: blockedData,
+                tension: 0.5,
+                pointStyle: 'line',
+            },
+        ],
+    };
 
     const depth = {
         labels,
         datasets: [
             {
-                label: 'Depth of query',
+                type: 'line' as const,
+                label: 'Depth',
+                backgroundColor: 'rgba(75, 192, 192, 0.8)',
                 data: depthData,
-                borderColor: 'rgb(75, 192, 192)',
-                backgroundColor: 'rgb(75, 192, 192)',
+                borderColor: 'rgba(75, 192, 192, 0.8)',
+                borderWidth: 2,
                 tension: 0.5,
                 pointStyle: 'line',
             },
@@ -122,10 +178,11 @@ const ChartBox: React.FC<IProps> = ({ queries }) => {
         labels,
         datasets: [
             {
-                label: 'Time to execute query',
+                type: 'line' as const,
+                label: 'Complexity',
+                backgroundColor: 'rgba(255, 255, 0, 0.5)',
+                borderColor: 'rgba(255, 255, 0, 0.5)',
                 data: complexityData,
-                borderColor: 'rgb(53, 162, 235)',
-                backgroundColor: 'rgb(53, 162, 235)',
                 tension: 0.5,
                 pointStyle: 'line',
             },
@@ -135,97 +192,101 @@ const ChartBox: React.FC<IProps> = ({ queries }) => {
     const data = {
         labels,
         datasets: [
-            // {
-            //     type: 'line' as const,
-            //     label: 'Time',
-            //     borderColor: 'rgb(255, 99, 132)',
-            //     borderWidth: 2,
-            //     fill: false,
-            //     data: timeFromProjectProps,
-            // },
-            {
-                type: 'line' as const,
-                label: 'Depth',
-                backgroundColor: 'rgb(75, 192, 192)',
-                data: depthData,
-                borderColor: 'rgb(75, 192, 192)',
-                borderWidth: 2,
-                tension: 0.5,
-                pointStyle: 'line',
-            },
-            {
-                type: 'line' as const,
-                label: 'Complexity',
-                backgroundColor: 'rgb(53, 162, 235)',
-                data: complexityData,
-                tension: 0.5,
-                pointStyle: 'line',
-            },
+            complexity.datasets[0],
+            depth.datasets[0],
+            blocked.datasets[0],
+            tokens.datasets[0],
         ],
     };
 
     const [style, setStyle] = useState<ISState['style']>({
-        // chartOne: 'block',
+        chartOne: 'none',
         chartTwo: 'none',
         chartThree: 'none',
-        chartFour: 'block',
+        chartFour: 'none',
+        chartFive: 'block',
     });
-
-    // const chartOneFn = () => {
-    //     setStyle({
-    //         ...style,
-    //         chartOne: 'block',
-    //         chartTwo: 'none',
-    //         chartThree: 'none',
-    //         chartFour: 'none',
-    //     });
-    // };
+    const chartOneFn = () => {
+        setStyle({
+            ...style,
+            chartOne: 'block',
+            chartTwo: 'none',
+            chartThree: 'none',
+            chartFour: 'none',
+            chartFive: 'none',
+        });
+    };
     const chartTwoFn = () => {
         setStyle({
             ...style,
-            // chartOne: 'none',
+            chartOne: 'none',
             chartTwo: 'block',
             chartThree: 'none',
             chartFour: 'none',
+            chartFive: 'none',
         });
     };
     const chartThreeFn = () => {
         setStyle({
             ...style,
-            // chartOne: 'none',
+            chartOne: 'none',
             chartTwo: 'none',
             chartThree: 'block',
             chartFour: 'none',
+            chartFive: 'none',
         });
     };
     const chartFourFn = () => {
         setStyle({
             ...style,
-            // chartOne: 'none',
+            chartOne: 'none',
             chartTwo: 'none',
             chartThree: 'none',
             chartFour: 'block',
+            chartFive: 'none',
+        });
+    };
+    const chartFiveFn = () => {
+        setStyle({
+            ...style,
+            chartOne: 'none',
+            chartTwo: 'none',
+            chartThree: 'none',
+            chartFour: 'none',
+            chartFive: 'block',
         });
     };
     return (
         <div id="chartBoxInside">
             <div className="projectSelector">
-                {/* <button onClick={() => chartOneFn()} className="chartBtn" type="button">
-                    Today
-                </button> */}
-                <button onClick={() => chartTwoFn()} className="chartBtn" type="button">
-                    Last 7 Days
+                <button onClick={() => setTimeRangeDays(30)} className="chartBtn" type="button">
+                    Last 30 Days
                 </button>
-                <button onClick={() => chartThreeFn()} className="chartBtn" type="button">
-                    Last 6 Months
+                <button onClick={() => setTimeRangeDays(90)} className="chartBtn" type="button">
+                    Last 90 Days
                 </button>
-                <button onClick={() => chartFourFn()} className="chartBtn" type="button">
-                    YTD
+                <button onClick={() => setTimeRangeDays(180)} className="chartBtn" type="button">
+                    Last 180 Days
+                </button>
+                <button onClick={() => setTimeRangeDays(360)} className="chartBtn" type="button">
+                    Last 360 Days
+                </button>
+                <button onClick={() => setSmoothingFactor(1)} className="chartBtn" type="button">
+                    Smooth 1
+                </button>
+                <button onClick={() => setSmoothingFactor(2)} className="chartBtn" type="button">
+                    Smooth 2
+                </button>
+                <button onClick={() => setSmoothingFactor(4)} className="chartBtn" type="button">
+                    Smooth 3
+                </button>
+                <button onClick={() => setSmoothingFactor(8)} className="chartBtn" type="button">
+                    Smooth 4
                 </button>
             </div>
-            {/* <div className="chartOne chartVisual" style={{ display: style.chartOne }}>
-                <Line options={options} data={time} />
-            </div> */}
+            <div className="chartOne chartVisual" style={{ display: style.chartOne }}>
+                <Line options={options} data={tokens} />
+            </div>
             <div className="chartTwo chartVisual" style={{ display: style.chartTwo }}>
                 <Line options={options} data={depth} />
             </div>
@@ -233,12 +294,15 @@ const ChartBox: React.FC<IProps> = ({ queries }) => {
                 <Line options={options} data={complexity} />
             </div>
             <div className="chartFour chartVisual" style={{ display: style.chartFour }}>
+                <Chart type="bar" options={options} data={blocked} />
+            </div>
+            <div className="chartFive chartVisual" style={{ display: style.chartFive }}>
                 <Chart type="bar" data={data} />
             </div>
             <div className="projectSelector">
-                {/* <button onClick={() => chartOneFn()} className="chartBtn" type="button">
-                    Time
-                </button> */}
+                <button onClick={() => chartOneFn()} className="chartBtn" type="button">
+                    Tokens
+                </button>
                 <button onClick={() => chartTwoFn()} className="chartBtn" type="button">
                     Depth
                 </button>
@@ -246,6 +310,9 @@ const ChartBox: React.FC<IProps> = ({ queries }) => {
                     Complexity
                 </button>
                 <button onClick={() => chartFourFn()} className="chartBtn" type="button">
+                    Blocked
+                </button>
+                <button onClick={() => chartFiveFn()} className="chartBtn" type="button">
                     Combined
                 </button>
             </div>
