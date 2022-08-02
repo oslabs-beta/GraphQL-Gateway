e3b784149659c8fb6af2b5acbdc8adf53a7edc94
Stephan Halarewicz <shalarewicz@gmail.com>
2022-08-02 18:37:17 -0400

    updated project config for better module support

--- server/models/Project.ts
+++ server/models/Project.ts
@@ -1,17 +1,17 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 
-const RateLimitOption = new Schema({
+const RateLimitOption = new mongoose.Schema({
     refillRate: { type: Number, required: true, default: 0 },
     windowSize: { type: Number, required: true, default: 0 },
     capacity: { type: Number, required: true },
 });
 
-const RateLimiterConfig = new Schema({
+const RateLimiterConfig = new mongoose.Schema({
     type: { type: String, required: true },
     options: { type: RateLimitOption, required: true },
 });
 
-const projectSchema = new Schema({
+const projectSchema = new mongoose.Schema({
     userID: { type: String, required: true },
     name: { type: String, required: true },
     apiKey: { type: String, required: true },
