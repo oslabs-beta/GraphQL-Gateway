eeb2e2b1132e7b2ba488a64b522cbef3808cb56d
Stephan Halarewicz <shalarewicz@gmail.com>
2022-08-04 17:34:39 -0400

    revert navbar

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -5,12 +5,12 @@ import { useAuth } from '../auth/AuthProvider';
 
 export default function Navbar() {
     const navigate = useNavigate();
-    const { user, setUser } = useAuth();
+    const { user, setUser, loading } = useAuth();
 
     const [show, setShow] = useState('flex');
     const scrollPos = useRef(window.scrollY);
 
-    const handleLogout = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const logout = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         setUser(null);
         localStorage.removeItem('session-token');
@@ -78,31 +78,27 @@ export default function Navbar() {
                 >
                     About
                 </Link>
-                {user && (
-                    <Link to="/dashboard" type="submit" className="linkBtn">
-                        Dashboard
-                    </Link>
-                )}
-                <div
-                    id="side-navbar"
-                    style={{
-                        display: show,
-                    }}
-                >
-                    {user ? (
-                        <Link to="/logout" type="submit" className="linkBtn" onClick={handleLogout}>
-                            Logout
-                        </Link>
-                    ) : (
-                        <>
-                            <Link to="/login" type="submit" className="linkBtn">
-                                Login
-                            </Link>
-                            <Link to="/signup" type="submit" className="linkBtn">
-                                Signup
-                            </Link>
-                        </>
-                    )}
+                <div id="side-navbar" style={{ display: show }}>
+                    {loading === false &&
+                        (user === null ? (
+                            <>
+                                <Link to="/login" type="submit" className="linkBtn">
+                                    Login
+                                </Link>
+                                <Link to="/signup" type="submit" className="linkBtn">
+                                    Signup
+                                </Link>
+                            </>
+                        ) : (
+                            <>
+                                <Link to="/dashboard" type="submit" className="linkBtn">
+                                    Dashboard
+                                </Link>
+                                <button className="linkBtn" type="button" onClick={logout}>
+                                    Logout
+                                </button>
+                            </>
+                        ))}
                 </div>
             </div>
         </div>
