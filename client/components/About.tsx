9879b147a495b8140be3bd211e4df247aa277427
Yufei Wu <yufeiwu@Yufeis-MacBook-Pro.local>
2022-08-03 18:08:01 -0700

    readme updated

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default function About() {
-    return (
-        <div className="container">
-
-        </div>
-    );
+    return <div className="container" />;
 }
