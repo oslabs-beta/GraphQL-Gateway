f32f6380148ee0adfb7bd00c536a5da53240e4ae
Stephan Halarewicz <shalarewicz@gmail.com>
2022-08-06 16:36:53 -0400

    updated mongoose types. issue will be created to refactor resolvers with proper mongoose typing and ensuring correct return values.

--- @types/resolver.d.ts
+++
@@ -1,5 +0,0 @@
-import { Document } from 'mongoose';
-
-export type MongoUser = User & Document;
-export type MongoProject = Project & Document;
-export type MongoProjectQuery = ProjectQuery & Document;
