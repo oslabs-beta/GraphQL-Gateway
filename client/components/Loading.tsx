0d61af3b553e8662ded445fefa6a1a8122062c10
Milos Popovic <milos_popovic@outlook.com>
2022-08-15 14:26:04 -0400

    fixed loading not being centered issue

--- client/components/Loading.tsx
+++ client/components/Loading.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Loading() {
     return (
         <div id="loading">
-            <div>Loading...</div>
+            <div>
+                <h3>Loading...</h3>
+            </div>
         </div>
     );
 }
